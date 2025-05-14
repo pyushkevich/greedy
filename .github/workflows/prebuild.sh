592df6be1bff23bc205a3cfb8196ecb99588480d
sarthakpati <sarthak.pati@hotmail.com>
2025-05-13 22:19:43 -0400

    Enable wrapping build option in prebuild script

--- .github/workflows/prebuild.sh
+++ .github/workflows/prebuild.sh
@@ -87,6 +87,7 @@ cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=./install \
     -DGREEDY_BUILD_LMSHOOT=ON \
+    -DGREEDY_BUILD_WRAPPING=ON \
     -DCMAKE_PREFIX_PATH="$PWD/install" \
     -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
     -DVTK_DIR=$PWD/install/vtk/vtk-9.3.1.data/headers/cmake \
